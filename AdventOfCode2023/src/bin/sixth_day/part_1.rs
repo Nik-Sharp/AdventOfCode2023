a03091ee0cb0bd2031fc3953112696d78016adef
Nik-Sharp <96154725+Nik-Sharp@users.noreply.github.com>
2023-12-06 10:59:08 -0500

    Day 6
    Due to yesterday's challenge, I know that iterating through each possible option was likely going to not work, so I looked at the problem and noticed that the graph looked like a parabola, so I tested an equation in Desmos and got it to work.
    
    Because of my optimization for Part 1, Part 2 was a breeze, as I only needed to change the parser.

---
+++ AdventOfCode2023/src/bin/sixth_day/part_1.rs
@@ -0,0 +1,60 @@
+use core::panic;
+use std::fs;
+
+const ELIPSION : f64 = 0.000001;
+
+fn quadratic_equation(a: f64, b: f64, c: f64) -> (f64, f64) {
+    let sqrt = ((b * b) - (4.0 * a * c)).sqrt();
+    ((-b + sqrt) / (2.0 * a), (-b - sqrt) / (2.0 * a))
+}
+#[derive(Debug)]
+struct Race {
+    pub length: f64,
+    pub record_distance: f64,
+}
+
+impl Race {
+    pub fn get_amount_possible(&self) -> u32 {
+        let x_intercepts = quadratic_equation(-1.0, self.length, -self.record_distance);
+
+        let mut result = x_intercepts.1.ceil() as u32 - x_intercepts.0.ceil() as u32;
+
+        if ((x_intercepts.1  - x_intercepts.0) - (result as f64)).abs() < ELIPSION {
+            result -= 1;
+        }
+
+        result
+    }
+}
+
+fn main() {
+    // Gets input from file
+    let full_input;
+
+    let mut inputs = match fs::read_to_string("input.txt") {
+        Ok(x) => {
+            full_input = x;
+            full_input.split("\n").map(|x| {
+                x.trim()
+                    .split_ascii_whitespace()
+                    .filter(|x| !x.is_empty() && x.chars().next().unwrap().is_ascii_digit())
+                    .map(|x| x.parse::<f64>().unwrap())
+            })
+        }
+
+        Err(x) => panic!("Error in reading, {}", x),
+    };
+    let mut distances = inputs.next().unwrap();
+    let mut record = inputs.next().unwrap();
+
+    let races = distances.map(|x| Race { length: x, record_distance: record.next().unwrap()});
+
+    // assumes there will be at least 1 race with a result
+    let mut result = 1;
+    for (_index, input) in races.enumerate() {
+        let possible = input.get_amount_possible();
+        result *= possible;
+    }
+
+    println!("{}", result);
+}
