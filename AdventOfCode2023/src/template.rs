ed4d789242652fe6c96f54bbbd38dc1122b3278c
Nik-Sharp <96154725+Nik-Sharp@users.noreply.github.com>
2023-12-28 15:07:17 -0500

    Commiting changes
    I'm repairing my laptop, want to backup everything in case something goes wrong

--- AdventOfCode2023/src/template.rs
+++ AdventOfCode2023/src/template.rs
@@ -1,22 +1,28 @@
 use core::panic;
 use std::fs;
 
-fn main() {
+use itertools::Itertools;
 
+fn main() {
     // Gets input from file
     let full_input;
 
-    let inputs = match fs::read_to_string("../input.txt") {
+    let inputs = match fs::read_to_string("input.txt") {
         Ok(x) => {
             full_input = x;
-            full_input.split("\n").map(|x| x.trim())
-        },
-        Err(x) => panic!("Error in reading, {}", x)
+            full_input.split("\n").map(|x| x.trim()).collect_vec()
+        }
+        Err(x) => panic!("Error in reading, {}", x),
     };
 
-    
-    for (_index, input) in inputs.enumerate() {
+    let rocks = inputs.iter().enumerate().map(|(y, line)| {
+        line.chars()
+            .enumerate()
+            .map(move |(x, char)| if char == 'O' { Some((x, y)) } else { None })
+            .flatten()
+    }).group_by(|(x, y)| y);
+
+    for (_index, input) in inputs.iter().enumerate() {
         println!("{}", input);
     }
-    
 }
